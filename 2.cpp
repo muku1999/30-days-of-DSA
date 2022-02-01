cfa59f695bf5b39fee8fd429516ec5ef8de238d0
Mukesh Nayak <mukeshnk5335@gmail.com>
2022-02-01 22:29:06 +0530

    Second Day

--- 2.cpp
+++ 2.cpp
@@ -13,7 +13,7 @@ for(int i=0;i<n;i++){
 return false;
 
 
-//Kadaen's Algorithm or Find Largest sum contiguous Subarray
+//Kadane's Algorithm or Find Largest sum contiguous Subarray
 
 		1 	2 	3 	-2 	5 
 maxsum=		1	3	6	4	9
@@ -26,6 +26,7 @@ for(int i=0;i<n;i++){
 	maxsum = max(maxsum ,currsum);
 	}
 return maxsum;
+
 
 //using if statements
 
@@ -43,4 +44,90 @@ for(int i=0;i<n;i++){
 		}
 return maxsum;
 
+//Kadane's Algoritm or Maximum product in a subarray
+
+long long mx = arr[0];
+long long mn = arr[0];
+long long product = arr[0];
+for(int i =0;i<n;i++){
+	if(arr[i]<0){
+	swap(mx.mn);
+	}
+	mn = min(1LL * arr[i], mn * arr[i]);
+	mx = max(1LL * arr[i], mx * arr[i]);
+	
+	product = max(mn,mx);
+	}
+return product;
+	
+
+
+//Finding the factorial of very big number
+
+multiply(int size ,int res[],int n){
+	int carry =0;
+	for(int x =0;x<size;x++){
+		int product = res[x] * n +carry;
+		res[x] = product % 10;
+		carry = product/10;
+		}
+	while(carry){
+		res[size] = carry%10;
+		carry = carry/10;
+		size++;
+		}
+		}
+
+fact(N){
+	int res[];
+	size =1; res[0]=1;
+	for(i = 2 ;i <n; i+++){
+		size= multiply(size ,res, i)
+	}
+	}
+	
+	
+//Longest Consecutive Subsequence
+
+2	6	1	9	4	5	3 
+
+//ans = 5
+
+class Solution{
+  public:
+    //Function to return length of longest subsequence of consecutive integers.
+    int findLongestConseqSubseq(int arr[], int n)
+    {
+        //using a Set to store elements.
+        unordered_set<int> S;
+        int ans = 0;
+ 
+        //inserting all the array elements in Set.
+        for (int i = 0; i < n; i++)
+           S.insert(arr[i]);
+ 
+        //checking each possible sequence from the start.
+        for (int i = 0; i < n; i++)
+        {
+           //if current element is starting element of a sequence then only 
+           //we try to find out the length of sequence.
+           if (S.find(arr[i] - 1) == S.end())
+           {
+               
+                int j = arr[i];
+                //then we keep checking whether the next consecutive elements
+                //are present in Set and we keep incrementing the counter.
+                while (S.find(j) != S.end())
+                j++;
+ 
+            //storing the maximum count.
+            ans = max(ans, j - arr[i]);
+            }
+        }
+        //returning the length of longest subsequence
+        return ans;
+    }
+};
+	
+	
 
